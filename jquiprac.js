8ad1349f9364326f380780d89abccf0c6ec4835b
Siddharth Kapil <sid12kapil@gmail.com>
2016-05-21 05:21:12 -0500

    JQueryUI
    Page with JQuery

---
+++ jquiprac.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function() {
+    $( "input[type=submit], #aa, button" )
+      .button()
+      .click(function( event ) {
+        event.preventDefault();
+      });
+     $('accord').accordion(); 
+  });*/
+$(document).ready(function(){
+	$("#btn4").hide("slow");
+});
